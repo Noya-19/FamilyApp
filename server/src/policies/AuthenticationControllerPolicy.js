4009e5f7ec5e61939e1b6b8be3cdcc50482da167
kylechan19 <51379831+kylechan19@users.noreply.github.com>
2020-11-15 11:17:10 -0800

    Final changes before merge

--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -7,6 +7,9 @@ module.exports = {
             password: Joi.string().regex(
                 new RegExp('^[a-zA-Z0-9]{8,32}$')
             ),
+            // FamilyId: Joi.string().regex(
+            //     new RegExp('^[a-zA-Z0-9]{8}$')
+            // ),
             FamilyId: Joi.number(),
             firstname: Joi.string(),
             lastname: Joi.string()
@@ -29,6 +32,21 @@ module.exports = {
                             <br>
                             2. It must be at least 8 characters in length and not greater than 32 characters in length.`
                             
+                    })
+                    break
+                case 'FamilyId':
+                    res.status(400).send({
+                         error: 'You must provide a valid family code.'
+                    })
+                    break
+                case 'firstname':
+                    res.status(400).send({
+                        error: 'Invalid first name.'
+                })
+                    break
+                case 'lastname':
+                    res.status(400).send({
+                        error: 'Invalid last name.'
                     })
                     break
                 default:
