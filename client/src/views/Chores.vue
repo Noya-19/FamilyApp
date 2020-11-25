bab862993ab1d481ec826452ee17929590a9965e
josh-tech <joshjay98@gmail.com>
2020-11-25 12:46:05 -0800

    functional chores without multiuser interface

--- client/src/views/Chores.vue
+++ client/src/views/Chores.vue
@@ -4,12 +4,10 @@
         <div class="grid-container">
 
             <div class="left">
-                <table id="shopping-list-table" class="table table-condensed table-hover">
+                <table id="chores-list-table" class="table table-condensed table-hover">
                     <thead>
                         <tr>
-<!--                            <th>Quantity</th>
-                            <th>Item</th>
-                            <th>Actions</th>-->
+                            <th>Chores</th>
                         </tr>
                     </thead>
                     <tr v-for="(item, index) in choresList">
@@ -17,19 +15,39 @@
                             {{item.choresName}}
                         </td>
                         <td>
-                           <!-- <button type="button" class="deleteButton" @click="deleteChore(index)" ></button>-->
+                            <button type="button" class="completeButton" @click="completedChore(index,item)" >Completed</button>
                         </td>
                     </tr>
                 </table>
-                <input type="text" v-model="choresName" class="choreNameText" placeholder="enter chore" id="textbox">
+                
             </div>
             <div class="header"><h1>Chores</h1></div>
-            <div class="middle">Personal</div>
+            <div class="middle">
+                <table id="completed-list-table" class="table table-condensed table-hover">
+                    <thead>
+                        <tr>
+                            <th>Completed</th>
+                        </tr>
+                    </thead>
+                    <tr v-for="(item, index) in  completedList">
+                        <td>
+                            {{item.choresName}}
+                        </td>
+                    </tr>
+                </table>
+            </div>
             <div class="right">Up for Grabs</div>
 
             <div class="footer">
-                <button type="button" class="addButton" onclick="document.getElementById('textbox').value = ''"  @click="addChore"  >Add New Chore</button>
-
+                <button type="button" class="btn" @click="openForm()">Add New Chore</button>
+                <div class="form-popup" id="myForm">
+                    <form action="/action_page.php" class="form-container">
+                        <h1>Add New Chore</h1>
+                        <input type="text" v-model="choresName" class="choreNameText" placeholder="enter chore" id="textbox">
+                        <button type="button" class="btn" @click="addChore">Add New Chore</button>
+                        <button type="button" class="btn cancel" @click="closeForm()">Close</button>
+                    </form>
+                </div>
             </div>
         </div>
 
@@ -46,25 +64,42 @@ export default {
     data () {
         return {
             choresName: "",
-            checked: "",
+            assigned:"",
             choresList: [],
+            completedList: [],
         }
     },
     methods: {
         getAllFamilyChores () {
             
         },
+        blankInput() {
+            alert("Chore cannot be left blank");
+        },
         addChore() {
-            const choresName = this.choresName;
+            if (this.choresName == "") {
+                alert("CHore cannot be left blank");
+                return;
+            }
             console.log(this.choresName);
             this.choresList.push({
                 choresName: this.choresName,
-                checked:false
-            })
+            });
+            this.choresName = "";
         },
-        deleteChore(index) {
-            choresList.splice(index, 1);
-            console.log("inside deleteChore");
+        completedChore(index,item) {
+            console.log("inside completedChore");
+            this.choresList.splice(index, 1);
+            this.completedList.push({
+                choresName: item.choresName,
+            });
+            
+        },
+        openForm() {
+            document.getElementById("myForm").style.display = "block";
+        },
+        closeForm() {
+            document.getElementById("myForm").style.display = "none";
         },
     },
     async mounted () {
@@ -142,5 +177,70 @@ export default {
         border-color: black;
         border-width: 0.125rem;
     }
+    .completeButton {
+        background-color: blue;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+    }
 
+    /* The popup form - hidden by default */
+    .form-popup {
+        display: none;
+        position: fixed;
+        bottom: 0;
+        right: 15px;
+        border: 3px solid #f1f1f1;
+        z-index: 9;
+    }
+
+    /* Add styles to the form container */
+    .form-container {
+        max-width: 300px;
+        padding: 10px;
+        background-color: white;
+    }
+
+    /* Full-width input fields */
+    .form-container input[type=text], .form-container input[type=password] {
+        width: 100%;
+        padding: 15px;
+        margin: 5px 0 22px 0;
+        border: none;
+        background: #f1f1f1;
+    }
+
+    /* When the inputs get focus, do something */
+    .form-container input[type=text]:focus, .form-container input[type=password]:focus {
+        background-color: #ddd;
+        outline: none;
+    }
+
+    /* Set a style for the submit/login button */
+    .form-container .btn {
+        background-color: #4CAF50;
+        color: white;
+        padding: 16px 20px;
+        border: none;
+        cursor: pointer;
+        width: 100%;
+        margin-bottom: 10px;
+        opacity: 0.8;
+    }
+
+    /* Add a red background color to the cancel button */
+    .form-container .cancel {
+        background-color: red;
+    }
+
+    /* Add some hover effects to buttons */
+    .form-container .btn:hover, .open-button:hover {
+        opacity: 1;
+    }
 </style>
