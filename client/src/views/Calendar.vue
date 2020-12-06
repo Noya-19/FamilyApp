90ba96438ea6793fa76c51cc43d94e32c7b1f34b
josh-tech <joshjay98@gmail.com>
2020-12-06 12:51:52 -0800

    fixed calendar sizing

--- client/src/views/Calendar.vue
+++ client/src/views/Calendar.vue
@@ -265,7 +265,7 @@
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         color: #2c3e50;
         height: 67vh;
-        width: 90vw;
+        width: auto;
         margin-left: auto;
         margin-right: auto;
     }
